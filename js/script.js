85fd43d05d0134af15450b0693b0af76d2f30f5a
Jorge Cruz <jlcruz7125@gmail.com>
2020-03-16 17:59:24 -0400

    Design

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function Player(name, mark){
+    this.name = name;
+    this.mark = mark;
+
+    return {name, mark};
+}
+
+const Gameboard = (function () {
+    
+    const blocks = document.getElementsByClassName("play-block");
+
+    return blocks;
+
+})();
+
+
+const CoreLogic = (function(){
+
+    // 1 - Ask for Players
+    // 2 - Create Players
+
+})();
